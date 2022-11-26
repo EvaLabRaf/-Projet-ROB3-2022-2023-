34c037941396a886c7f7948b85e4099daacd87ab
LBlaze911 <113677370+LBlaze911@users.noreply.github.com>
2022-11-26 10:50:23 +0100

    1

--- Rapports de séances/Rapports_Hugo/Annexes/2022-11-25_CommandeMoteur/2022-11-25_CommandeMoteur.ino
+++ Rapports de séances/Rapports_Hugo/Annexes/2022-11-25_CommandeMoteur/2022-11-25_CommandeMoteur.ino
@@ -1,11 +1,37 @@
-int 
+//Moteurs Droit
+int PWDD = 3;
+int DIRD = 4;
+//Moteurs Gauche
+int PWDG = 5;
+int DIRG = 6;
+
 
 void setup() {
-  // put your setup code here, to run once:
-
+pinMode(PWDD, OUTPUT);
+pinMode(DIRD, OUTPUT);
+pinMode(PWDG, OUTPUT);
+pinMode(DIRG, OUTPUT);
+Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+Serial.println("Avance");
+digitalWrite(PWDD,HIGH);
+digitalWrite(DIRD,HIGH);
+digitalWrite(PWDG,HIGH);
+digitalWrite(DIRG,LOW);
+delay(5000);
+digitalWrite(PWDD, LOW);
+digitalWrite(PWDG, LOW);
+delay(2000);
 
+Serial.println("Recule");
+digitalWrite(PWDD,HIGH);
+digitalWrite(DIRD,LOW);
+digitalWrite(PWDG,HIGH);
+digitalWrite(DIRG,HIGH);
+delay(5000);
+digitalWrite(PWDD, LOW);
+digitalWrite(PWDG, LOW);
+delay(2000);
 }
