4c9876e8f396697a79656ec540787d3febb14d14
LBlaze911 <113677370+LBlaze911@users.noreply.github.com>
2022-12-16 08:45:31 +0100

    1

--- Rapports de séances/Rapports_Hugo/Annexes/CodeTestAdafruit/Capteurdistance.cpp
+++ Rapports de séances/Rapports_Hugo/Annexes/CodeTestAdafruit/Capteurdistance.cpp
@@ -16,7 +16,7 @@ Capteurdistance::Capteurdistance(){
 int Capteurdistance::distance() {
   Serial.println("test distance");
   VL53L0X_RangingMeasurementData_t measure;
-  //rangingTest(&measure, false);
+  rangingTest(&measure, false);
   //return measure.RangeMilliMeter;
   return 1;
   }
